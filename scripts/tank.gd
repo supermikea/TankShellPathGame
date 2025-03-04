4fa8c67eb8838493e61c8797812b1a49c029729e
Mike Wiegman Avila <mikewiegmanavila@keemail.me>
2025-03-04 21:01:09 +0100

    Add basic movement

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
 @export var is_player: bool = false
+@export var speed: float = 300.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
-	pass
+	# add the gravity
+	velocity.y += gravity * delta
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed # maybe add delta? idk needs testing
+	
+	move_and_slide()
