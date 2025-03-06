2ed844205711e101522c291cc662826ae1ea420c
noonehas <silvijn.vankomen@gmail.com>
2025-03-06 16:43:58 +0100

    Hot Fixed tank movement

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -10,6 +10,7 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	
 	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed # maybe add delta? idk needs testing
-	
+	velocity.x = direction * speed # maybe add delta? idk needs testing | silvijn: nee vgm heeft physics_process dat built in ofzo
+	print(velocity)
+	print(is_on_floor())
 	move_and_slide()
